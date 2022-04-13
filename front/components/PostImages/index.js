130b2543937be5c4dfa0de211bc43302e1c09721
Junmo <tauche.t3473@gmail.com>
2022-04-13 12:46:16 +0900

    thumb 이미지 경로 수정

--- front/components/PostImages/index.js
+++ front/components/PostImages/index.js
@@ -17,7 +17,7 @@ const PostImages = ({ images }) => {
   if(images.length === 1) {
     return (
       <>
-        <img src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img src={images[0].src.replace(/\/thumb\//, '/original/')} alt={images[0].src.replace(/\/thumb\//, '/original/')} onClick={onZoom} />
         {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}  
       </>
     );
@@ -26,8 +26,8 @@ const PostImages = ({ images }) => {
   if(images.length === 2) {
     return (
       <SecondImg>
-        <img src={images[0].src} alt={images[0].src} onClick={onZoom} />
-        <img src={images[1].src} alt={images[1].src} onClick={onZoom} />
+        <img src={images[0].src.replace(/\/thumb\//, '/original/')} alt={images[0].src.replace(/\/thumb\//, '/original/')} onClick={onZoom} />
+        <img src={images[1].src.replace(/\/thumb\//, '/original/')} alt={images[1].src.replace(/\/thumb\//, '/original/')} onClick={onZoom} />
         {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}  
       </SecondImg>
     );
@@ -35,7 +35,7 @@ const PostImages = ({ images }) => {
 
   return (
     <MoreImg>
-      <img src={images[0].src} alt={images[0].src} onClick={onZoom} />
+      <img src={images[0].src.replace(/\/thumb\//, '/original/')} alt={images[0].src.replace(/\/thumb\//, '/original/')} onClick={onZoom} />
       <div onClick={onZoom} className="moreBtn">
         <AiOutlinePlus />
         <span>{images.length - 1}개의 이미지 더 보기</span>
