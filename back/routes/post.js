bc8e288815d925da3f7ff420621d088526585a4f
Junmo <tauche.t3473@gmail.com>
2022-04-13 11:40:27 +0900

    lambda thumb, original 설정

--- back/routes/post.js
+++ back/routes/post.js
@@ -38,7 +38,7 @@ const upload = multer({
 });
 router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {
   console.log(req.files);
-  res.json(req.files.map((v) => v.location));
+  res.json(req.files.map((v) => v.location.replace(/\/original\//, '/thumb/')));
 });
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
