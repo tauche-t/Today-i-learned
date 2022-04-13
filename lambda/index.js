8fe8d9edc72acfc4eacf54f7f8fc5ff2efac11b7
Junmo <tauche.t3473@gmail.com>
2022-04-13 12:13:21 +0900

    lambda 수정

--- lambda/index.js
+++ lambda/index.js
@@ -4,8 +4,8 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Records[0].s3.object.key);
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
   console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
@@ -15,18 +15,19 @@ exports.handler = async (event, context, callback) => {
   try {
     const s3Object = await s3.getObject({ Bucket, Key }).promise();
     console.log('original', s3Object.Body.length);
-    const resizedImage = await sharp(s3Object.Body).resize(400, 400, { fit: 'inside' }).toFormat(requiredFormat).toBuffer();
+    const resizedImage = await sharp(s3Object.Body)
+      .resize(400, 400, { fit: 'inside' })
+      .toFormat(requiredFormat)
+      .toBuffer();
     await s3.putObject({
       Bucket,
       Key: `thumb/${filename}`,
       Body: resizedImage,
     }).promise();
-
     console.log('put', resizedImage.length);
-
     return callback(null, `thumb/${filename}`);
   } catch (error) {
-    console.error(erorr);
-    return callback(error);  // passport의 done과 유사
+    console.error(error)
+    return callback(error);
   }
 }
